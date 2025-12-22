b6a6e313e0958bf35e891dda4b5af9d7af579a23
Jyotirmoy Bandyopadhayaya <jbandyopadhayaya@gmail.com>
2025-12-22 18:43:44 +0000

    ci: added docker watch config

--- internal/transport/http/handler/health_handler.go
+++ internal/transport/http/handler/health_handler.go
@@ -4,9 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func HealthHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"name":   "Git Server API",
-			"status": "healthy",
-		})
+		c.Writer.Write([]byte("OK!"))
 	}
 }
